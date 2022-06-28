5fc5d98c4faa5838ca989778c316dba98642e7f8
Lu Tuan Dat <dattuan@zenfolio.com>
2022-06-28 19:38:20 +0700

    Update code

--- buyOnBot.js
+++ buyOnBot.js
@@ -2,10 +2,12 @@ import nacl from 'tweetnacl';
 import * as https from 'https';
 import ReadLine from 'readline';
 import fs from 'fs';
+import axios from 'axios';
+import fetch from "node-fetch";
 
 let rawdata = fs.readFileSync('file.json');
 let item = JSON.parse(rawdata);
-const { itemName, offSeS, itemId, isBuySuccess, maxPrice } = item;
+const { itemName, offSeS, itemId } = item;
 
 const options = {
     timeZone: 'Asia/Bangkok',
@@ -62,16 +64,16 @@ function hex2ascii(hexx) {
 }
 
 // insert your api keys
-const publicKey = '093a0fbc0c4b137080c4ed41af6b0f6b3ac6fe4098f6382ae059030fe01aa245';
+const publicKey = '773ab3f90a39efb38e93a5ecc10c236a99266fa643bc9398da4f2d63018864af';
 const secretKey =
-  '80d7c7c9f6c7bcad071041e012a897ab8c1ac9831f0e7ee47219549a25191e7b093a0fbc0c4b137080c4ed41af6b0f6b3ac6fe4098f6382ae059030fe01aa245';
+  'f5e44cad091c9b6f3d526cf21e53afea9b72a30c9e6c43b320481522dd09ad3a773ab3f90a39efb38e93a5ecc10c236a99266fa643bc9398da4f2d63018864af';
 
-const host = 'api.dmarket.com';
+const host = ' ';
 const readline = ReadLine.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-let limitItemStack = 999;
+let limitItemStack = 10;
 let itemFound = {};
 // let data;
 export const encodeQuery = (obj) => {
@@ -81,52 +83,32 @@ export const encodeQuery = (obj) => {
     .join('&');
 };
 
+const createQueryString = (data) => {
+  return Object.keys(data).map(key => {
+    let val = data[key]
+    if (val !== null && typeof val === 'object') val = createQueryString(val)
+    return `${key}=${encodeURIComponent(`${val}`.replace(/\s/g, '_'))}`
+  }).join('&')
+}
+
+
 function getListItemByName(itemName) {
   const objectParams = {
-    Title: itemName,
-    Limit: limitItemStack
+    gameId: 'a8db',
+    title: itemName,
+    limit: limitItemStack,
+    currency: 'USD',
+    orderBy: 'createdAt',
+    orderDir: 'desc',
+    offset: 0,
+    priceFrom: 0,
+    priceTo: 0,
   };
-  const timestamp = Math.floor(new Date().getTime() / 1000);
-  const stringToSign =
-    'GET' + `/exchange/v1/offers-by-title` + JSON.stringify(objectParams) + timestamp;
-  const signature = sign(stringToSign);
-  const requestOptions = {
-    host: host,
-    path: `/exchange/v1/offers-by-title?${encodeQuery(objectParams)}`,
-    method: 'GET',
-    headers: {
-      'X-Api-Key': publicKey,
-      'X-Request-Sign': 'dmar ed25519 ' + signature,
-      'X-Sign-Date': timestamp,
-      'Content-Type': 'application/json'
-    }
-  };
-  // 'X-RateLimit-Limit-Second': 6,
-  // 'X-RateLimit-Remaining-Second': 5,
-  // 'RateLimit-Remaining': 5,
-  // 'RateLimit-Limit': 6,
-  // 'RateLimit-Reset': 1
-
-  // you can use a more high-level wrapper for requests instead of native https.request
-  // check https://github.com/axios/axios as an example
-  return new Promise(function (resolve, reject) {
-    const request = https.request(requestOptions, (response) => {
-      let body = [];
-      response.on('data', function (chunk) {
-        body.push(chunk);
-      });
-      // resolve on end
-      response.on('end', function () {
-        try {
-          body = JSON.parse(Buffer.concat(body).toString());
-        } catch (e) {
-          reject('getListItemByName', e);
-        }
-        resolve(body);
-      });
-    });
-    request.end();
-  });
+  return axios({
+    url: `https://api.dmarket.com/exchange/v1/market/items${encodeQuery(objectParams)}`,
+    method: 'get',
+    maxRedirects: 0,
+  })
 }
 
 function sign(string) {
@@ -137,57 +119,26 @@ function sign(string) {
   return byteToHexString(signatureBytes).substr(0, 128);
 }
 
-function makeOfferAndBuy(requestOptions, offer) {
-  return new Promise(function (resolve, reject) {
-    const result = https.request(requestOptions, (response) => {
-      console.log('statusCode:', response.statusCode);
-      response.on('data', (responseBodyBytes) => {
-        try {
-          console.log(hex2ascii(byteToHexString(responseBodyBytes)));
-        } catch (e) {
-          console.log('makeOfferAndBuy trong promise', e);
-          reject(e);
-        }
-        resolve(responseBodyBytes);
-      });
-    });
-
-    result.on('error', (e) => {
-      console.error('makeOfferAndBuy', e);
-    });
-
-    result.write(offer);
-    result.end();
-  });
-}
-
-let isRunFindSpecifyItem = true;
 async function findSpecifyItem() {
-  isRunFindSpecifyItem = false;
   try {
-    const result = await getListItemByName(itemName);
-    limitItemStack = result.total.offers;
-    if(result.objects.length > 0) {
-      itemFound = result.objects.find((item) => {
-        return item.extra.floatValue && item.extra.floatValue.toString() === itemId;
-      });
-      if (itemFound === undefined) {
-        itemFound = {};
-      } 
-      if (Object.keys(itemFound).length > 0) {
-        console.log('Item found! Making offer to buy...');
-        buyItem();
-      } else {
-        console.log('\x1b[36m%s\x1b[0m', getDate(), 'item not found');
-      }
-    }
-    else {
-      console.log('\x1b[36m%s\x1b[0m', getDate(), 'List is null');
-    }
+    const response = await getListItemByName(itemName);
+    console.log(response);
+    // if(data.length > 0) {
+    //   itemFound = data.find((item) => {
+    //     return item.extra.floatValue && item.extra.floatValue.toString() === itemId;
+    //   });
+    //   if (itemFound && Object.keys(itemFound).length > 0) {
+    //     console.log('Item found! Making offer to buy...');
+    //     buyItem();
+    //   } else {
+    //     console.log('\x1b[36m%s\x1b[0m', getDate(), 'item not found');
+    //   }
+    // }
+    // else {
+    //   console.log('\x1b[36m%s\x1b[0m', getDate(), 'List is null');
+    // }
   } catch (e) {
-    console.log('findSpecifyItem', e);
-  } finally {
-    isRunFindSpecifyItem = true;
+    console.log('Cannot find item: ', e);
   }
 }
 
@@ -221,39 +172,45 @@ async function buyItem() {
         'Content-Type': 'application/json'
       }
     };
-    if (Number(itemFound.price.USD) > Number(maxPrice)) {
-      console.log('Item is overpaid:', Number(itemFound.price.USD));
-    }else {
-      const result = await makeOfferAndBuy(requestOptions, offer);
-      if (result) {
-        const data = JSON.stringify({ itemName, offSeS, itemId, isBuySuccess: true });
-        fs.writeFileSync('file.json', data);
-      }
+    const result = await makeOfferAndBuy(requestOptions, offer);
+    if (result) {
+      const data = JSON.stringify({ itemName, offSeS, itemId, isBuySuccess: true });
+      fs.writeFileSync('file.json', data);
     }
   } catch (e) {
     console.log('buyItem', e);
   }
 }
 
-function askToBuyItem() {
-  try {
-    setInterval(() => {
-      const obj = { itemName, offSeS, itemId, isBuySuccess, maxPrice };
-      if (isBuySuccess) {
-        console.log('buy success');
-      } else {
-        if (isRunFindSpecifyItem) {
-          try {
-            findSpecifyItem();
-          } catch (error) {
-            console.log('trong try catch', error);
-          }
+function makeOfferAndBuy(requestOptions, offer) {
+  return new Promise(function (resolve, reject) {
+    const result = https.request(requestOptions, (response) => {
+      console.log('statusCode:', response.statusCode);
+      response.on('data', (responseBodyBytes) => {
+        try {
+          console.log('Buy success', hex2ascii(byteToHexString(responseBodyBytes)));
+          itemFound = {};
+        } catch (e) {
+          console.log('makeOfferAndBuy trong promise', e);
+          reject(e);
         }
-      }
-    }, 1000);
-  } catch (error) {
-    console.log('askToBuyItem', error);
-  }
+        resolve(responseBodyBytes);
+      });
+    });
+
+    result.on('error', (e) => {
+      console.error('makeOfferAndBuy', e);
+    });
+
+    result.write(offer);
+    result.end();
+  });
+}
+
+function askToBuyItem() {
+  setInterval(() => {
+    findSpecifyItem();
+  }, 0);
 }
 
 askToBuyItem();
